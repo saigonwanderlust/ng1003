75192611e7920bf92c7d14ab7498cb4d35547763
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-17 17:48:00 +0700

    skills

--- src/app/components/person.component.ts
+++ src/app/components/person.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
         <div>
             <h3>Name: {{ name }}</h3>
             <p>Age: {{ age }}</p>
+            <div *ngIf="age >= 18">
+                <p>Ban da du tuoi.</p>
+            </div>
+            <div *ngFor="let skill of skills; let i = index">
+                <p class="text-info">{{ i + 1 }}. {{ skill.name }}: {{ skill.grade }}</p>
+            </div>
             <button class="btn btn-success" (click)="increase();">Increase</button>
             <button class="btn btn-warning" (click)="descrease();">Descrease</button>
         </div>
@@ -15,7 +21,16 @@ import { Component } from '@angular/core';
 export class PersonComponent {
     name = 'Teo Nguyen';
     age = 16;
-
+    skills: Skill[] = [
+        { name: 'NodeJS', grade: 9 },
+        { name: 'React', grade: 5 },
+        { name: 'Angular', grade: 7 },
+    ];
     increase() { this.age++;  }
     descrease() { this.age--;  }
+}
+
+interface Skill {
+    name: string;
+    grade: number;
 }
